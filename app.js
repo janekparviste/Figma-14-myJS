98fbcbf279632de849720371bec69cd6773d5474
JanekParviste <janek.parviste@tktk.ee>
2024-10-07 16:47:55 +0300

    close button

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const btnHamburger = document.querySelector('.hamburger');
 const menuPanel = document.querySelector('.menu-panel');
 const menuLinks = document.querySelectorAll('.menu-panel a');
+let close = document.querySelector('.close');
+
+close.addEventListener('click', () => {
+    menuPanel.classList.remove('active');
+});
 
 btnHamburger.addEventListener('click', () => {
     menuPanel.classList.toggle('active');
